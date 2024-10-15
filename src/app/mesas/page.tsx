5ca06bf4a840802fd5f596ce6ff99b9c44bdfb57
Jose Italo <jose_italo_PF021@hotmail.com>
2024-10-15 17:18:58 -0300

    Deploy1
    Inicio do projeto escudo.m( sem nome oficial ainda )

---
+++ src/app/mesas/page.tsx
@@ -0,0 +1,7 @@
+export default function Mesas(){
+    return(
+        <>
+            <h1> Mesas </h1>
+        </>
+    )
+}
---
+++ src/app/teste/page.tsx
@@ -0,0 +1,3 @@
+export default function TestePage(){
+    return <h1>Ola</h1>
+}
